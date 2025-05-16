e2df6f6c6fc9d8895c4e921f747adafce33c06b0
Jeremy Rand <jeremyrand@danwin1210.de>
2025-05-16 23:23:07 +0000

    Add pi meta-domain

--- server/server.go
+++ server/server.go
@@ -470,9 +470,21 @@ func (s *Server) lookupCert(req *http.Request) (certDer []byte, shortTerm bool, 
 		return nil, false, nil
 	}
 
-	tlsa, err := lookupDNS(req, domain)
+	tlsa, err := lookupPi(req, domain)
 	if err != nil {
 		return nil, false, err
+	}
+
+	if tlsa == nil {
+		tlsa, err := lookupDNS(req, domain)
+		if err != nil {
+			return nil, false, err
+		}
+	}
+
+	// TODO: backport to previous commit
+	if tlsa == nil {
+		return nil, false, nil
 	}
 
 	safeCert, err := safetlsa.GetCertFromTLSA(domain, tlsa, s.tldCert, s.tldPriv)
